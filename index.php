12fc92a3292893dfb4aed4da445a81a82270279f
Muvaffak Onus <onus.muvaffak@gmail.com>
2018-03-03 16:10:54 +0300

    Initial web page is created with Bootstrap.
    - Text area is added.
    - Buttons are added.
    - Description is there.

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Sticky Footer Template for Bootstrap</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/sticky-footer.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- Begin page content -->
+  <main role="main" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="mt-5">Pastify</h1>
+        <p class="lead">Paste whatever you need to the following box and share with your friends.</p>
+      </div>
+    </div>
+    <div class="form-group">
+      <textarea class="form-control" placeholder="Message"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <button class="btn">Clean</button>
+  </main>
+
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted">@2018 Muvaffak Onus</span>
+    </div>
+  </footer>
+  <script src="js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
